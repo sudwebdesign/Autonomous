9a1cf1bada6da2db57d710101248fadcba2bdf6b
Jo <jo@wildsurikat.com>
2014-06-03 00:28:23 +0200

    prepare validation and persona auth

--- js/persona.js
+++ js/persona.js
@@ -8,7 +8,15 @@ Persona = function(logged){
 	var initCALLED = false;
 	var loginCALL = function(){
 		initCALL();
-		navigator.id.request();
+		navigator.id.request({
+			siteName: 'UDAC66', //Plain text name of your site to show in the login dialog. Unicode and whitespace are allowed, but markup is not.
+			backgroundColor:'#ff9900',
+			//oncancel:function(){}, //invoked if the user refuses to share an identity with the site.
+			//privacyPolicy:'/Politique-Confidentialité', //Must be served over SSL. The termsOfService parameter must also be provided. Absolute path or URL to the web site's privacy policy. If provided, then termsOfService must also be provided. When both termsOfService and privacyPolicy are given, the login dialog informs the user that, by continuing, "you confirm that you accept this site's Terms of Use and Privacy Policy." The dialog provides links to the the respective policies.
+			//returnTo: document.location, //Absolute path to send new users to after they've completed email verification for the first time. The path must begin with '/'. This parameter only affects users who are certified by Mozilla's fallback Identity Provider. This value passed in should be a valid path which could be used to set window.location too.
+			//siteLogo: '/img/logo.png', //Must be served over SSL. Absolute path to an image to show in the login dialog. The path must begin with '/'. Larger images will be scaled down to fit within 100x100 pixels.
+			//termsOfService: 'Termes-Utilisation', Optional Must be served over SSL. The privacyPolicy parameter must also be provided. Absolute path or URL to the web site's terms of service. If provided, then privacyPolicy must also be provided. When both termsOfService and privacyPolicy are given, the login dialog informs the user that, by continuing, "you confirm that you accept this site's Terms of Use and Privacy Policy." The dialog provides links to the the respective policies. 
+		});
 	};
 	var logoutCALL = function(){
 		initCALL();
