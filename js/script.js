ff55f2e17a9a5c9510c51bf85cb4251ad128adb5
Jo <jo@wildsurikat.com>
2014-06-03 13:59:39 +0200

    navigator amelioration
    navigator ameliorations + control session bugfix + persona cleaning +
    responsive restructuration

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-$js(true,[
-	'jquery',
-	'jquery-ui/core',
-	'jquery-ui/widget'
-],function(){
+$js('jquery',function(){
 	var loc = document.location.pathname;
 	var i = loc.indexOf(':');
 	var i2 = loc.indexOf('|');
@@ -11,36 +7,12 @@ $js(true,[
 	else if(i!==-1)
 		loc = loc.substr(0,i2);
 	loc = decodeURIComponent(loc.substr(1));
-	//var li = $('body>nav>[is=dropdown]>li>a[href="'+loc+'"]').parent('li');
-	//li.addClass('active');
-	//li.parent('ul,[is=dropdown]').parent('li').addClass('active');
-	//var mls = 'body>nav>[is=dropdown]>li>a[rel!="external"][target!="_blank"]:not([href^="javascript:"]):not([is=collapser])';
-	//$(mls).on('click',function(){
-		//$(mls).removeClass('active');
-		//$(this).addClass('active');
-		//$('main').css('opacity',0.5);
-	//});
+	var li = $('body>nav>[is=dropdown]>li>a[href="'+loc+'"]').parent('li');
+	li.addClass('active');
+	li.parent('ul,[is=dropdown]').parent('li').addClass('active');
 	$(window).on('unload',function(){
 		$('main').css('opacity',0.5);
 	});
 });
-
-var ie = (function(){var undef,v = 3,div = document.createElement('div'),all = div.getElementsByTagName('i');while(div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',all[0]);return v>4?v:undef;}());
-if(ie&&ie<9)
-	$js(['html5shiv','local-storage.retro']);
-
-//if(typeof window.matchMedia=="undefined"&&typeof window.msMatchMedia=="undefined")
-	//$js('respond');
-	
-$js([
-	'jquery',
-	'local-storage',
-	'https://login.persona.org/include.js',
-	'persona',
-],function(){
-	$.getJSON('service/persona/email',function(email){
-		Persona(email);
-	});
-});
-
+<!--#include virtual="/js/retro.js" -->
 $css('print.min','print');
---
+++ x-dom/persona.js
@@ -0,0 +1,70 @@
+$js([
+	'jquery',
+	'local-storage',
+	'https://login.persona.org/include.js',
+],function(){
+	$.getJSON('service/persona/email',function(email){
+		//PERSONA
+		localStorage.setItem('current',1);
+		var currentUser = email,
+			loginBTN = $('a[is=persona]'),
+			logoutBTN = $('a.persona.logout');
+
+		var initCALLED = false;
+		var loginCALL = function(){
+			initCALL();
+			navigator.id.request({
+				siteName: 'UDAC66', //Plain text name of your site to show in the login dialog. Unicode and whitespace are allowed, but markup is not.
+				backgroundColor:'#ff9900',
+				//oncancel:function(){}, //invoked if the user refuses to share an identity with the site.
+				//privacyPolicy:'/Politique-Confidentialité', //Must be served over SSL. The termsOfService parameter must also be provided. Absolute path or URL to the web site's privacy policy. If provided, then termsOfService must also be provided. When both termsOfService and privacyPolicy are given, the login dialog informs the user that, by continuing, "you confirm that you accept this site's Terms of Use and Privacy Policy." The dialog provides links to the the respective policies.
+				//returnTo: document.location, //Absolute path to send new users to after they've completed email verification for the first time. The path must begin with '/'. This parameter only affects users who are certified by Mozilla's fallback Identity Provider. This value passed in should be a valid path which could be used to set window.location too.
+				//siteLogo: '/img/logo.png', //Must be served over SSL. Absolute path to an image to show in the login dialog. The path must begin with '/'. Larger images will be scaled down to fit within 100x100 pixels.
+				//termsOfService: 'Termes-Utilisation', Optional Must be served over SSL. The privacyPolicy parameter must also be provided. Absolute path or URL to the web site's terms of service. If provided, then privacyPolicy must also be provided. When both termsOfService and privacyPolicy are given, the login dialog informs the user that, by continuing, "you confirm that you accept this site's Terms of Use and Privacy Policy." The dialog provides links to the the respective policies. 
+			});
+		};
+		var logoutCALL = function(){
+			initCALL();
+			navigator.id.logout();
+		};
+		var logonCALL = function(currentUser){
+			localStorage.setItem('currentUser',currentUser);
+			loginBTN.data('origin',loginBTN.html());
+			loginBTN.html(currentUser);
+			loginBTN.off('click',loginCALL);
+		};
+		var logoffCALL = function(){
+			currentUser = false;
+			localStorage.removeItem('currentUser');
+			loginBTN.html(loginBTN.data('origin'));
+			loginBTN.next('ul').addClass('hide');
+			loginBTN.on('click',loginCALL);
+		};
+		var initCALL = function(){
+			if(initCALLED)
+				return;
+			initCALLED = true;
+			navigator.id.watch({
+				onlogin: function(as){
+					$.post('service/persona/login',{assertion:as},function(login){
+						if(login.status==='okay')
+							logonCALL(login.email);
+					});
+				},
+				onlogout: function () {
+					$.get('service/persona/logout',function(){
+						logoffCALL();
+					});
+				},
+				onready: function(){
+				}
+			});				
+		};
+		loginBTN.on('click',loginCALL);
+		logoutBTN.on('click',logoutCALL);
+		if(currentUser)
+			logonCALL(currentUser);
+		else if(!localStorage.getItem('current'))
+			initCALL();
+	});
+});
