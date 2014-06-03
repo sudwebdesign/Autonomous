20170935f07ca80c7858309326197c24b55c3883
Jo <jo@wildsurikat.com>
2014-06-03 19:08:52 +0200

    prepare list templates

---
+++ present/liste.php
@@ -0,0 +1,17 @@
+<?php namespace present;
+use view;
+use model;
+class liste extends \present{
+	static function compileVars(&$vars=array()){
+		
+	}
+	static function compileElement(){
+		
+	}
+	static function exec(){
+			
+	}	
+	static function execVars(&$vars=array()){
+		
+	}
+}
