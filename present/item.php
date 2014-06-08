f4a85d2319f7a57794a02e4b60bf3e577eb66dd7
Jo <jo@wildsurikat.com>
2014-06-08 19:04:35 +0200

    adding item couch to present logic

---
+++ present/item.php
@@ -0,0 +1,23 @@
+<?php namespace present;
+use view;
+use model;
+use surikat\control\ArrayObject;
+class item extends \present{
+	static $taxonomy;
+	static function compileVars(&$vars=array()){
+		
+	}
+	static function compileElement(){
+		
+	}
+	static function exec(){
+		static::$taxonomy = end(self::$options['namespaces']);
+	}	
+	static function execVars(&$vars=array()){
+		$table = static::$taxonomy;
+		
+		return array(
+			'taxonomy'=>static::$taxonomy,
+		);
+	}
+}
