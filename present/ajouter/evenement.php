8d6a8d0b7ae0c78d7f70d759a4219352e7d775e9
Jo <jo@wildsurikat.com>
2014-06-12 15:51:01 +0200

    datetime format bugfix

--- present/ajouter/evenement.php
+++ present/ajouter/evenement.php
@@ -4,14 +4,12 @@ use model\R;
 use control\dates;
 use control\filter;
 final class evenement extends \present\ajouter{
-	static function exec(){
-
+	function POST_Specifications($bean){
+		$this->POST_Dates($bean);
 	}
-	static function POST_Specifications($bean){
-		self::POST_Dates($bean);
-	}
-	protected static $datetimeCombine = true;
-	protected static function POST_Dates($bean){
+	
+	var $datetimeCombine = true;
+	function POST_Dates($bean){
 		static $vars = array('date_start','date_end','time_start','time_end');
 		if(!isset($_POST['xownDate']))
 			return;
@@ -29,14 +27,19 @@ final class evenement extends \present\ajouter{
 				$bean->error('xownDate[date_end]'.$se,'invalid format');
 			if(!dates::validate_time($time_end))
 				$bean->error('xownDate[time_end]'.$se,'invalid format');
-		}		
+		}
 		if($multi){
 			foreach(array_keys($date_start) as $i){
-				if(self::$datetimeCombine){
+				if($this->datetimeCombine){
 					$date = array(
-						'start'=>$date_start[$i].' '.(isset($time_start[$i])?$time_start[$i]:'00:00:00'),
-						'end'=>$date_end[$i].' '.(isset($time_end[$i])?$time_end[$i]:'00:00:00'),
+						'start'=>@$date_start[$i]?$date_start[$i].' '.(@$time_start[$i]?$time_start[$i]:'00:00:00'):null,
+						'end'=>@$date_end[$i]?$date_end[$i].' '.(@$time_end[$i]?$time_end[$i]:'00:00:00'):null,
 					);
+					if(!dates::validate_datetime($date['start']))
+						$bean->error('xownDate[date_start]'.$se,'missing or invalid format');
+					if($date['end']&&!dates::validate_datetime($date['end']))
+						$bean->error('xownDate[date_end]'.$se,'missing or invalid format');
+					//var_dump($date);exit;
 				}
 				else{
 					$date = array();
@@ -47,7 +50,7 @@ final class evenement extends \present\ajouter{
 			}
 		}
 		else{
-			if(self::$datetimeCombine){
+			if($this->datetimeCombine){
 				$date = array(
 					'start'=>$date_start[$i].' '.(isset($time_start[$i])?$time_start[$i]:'00:00:00'),
 					'end'=>$date_end[$i].' '.(isset($time_end[$i])?$time_end[$i]:'00:00:00'),
