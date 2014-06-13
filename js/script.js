aad3d9493f3710a8f22201efff911456f78eed28
Jo <jo@wildsurikat.com>
2014-06-13 11:07:19 +0200

    active menu handle correctly

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $js('jquery',function(){
 	var i2 = loc.indexOf('|');
 	if(i!==-1)
 		loc = loc.substr(0,i);
-	else if(i!==-1)
+	else if(i2!==-1)
 		loc = loc.substr(0,i2);
 	loc = decodeURIComponent(loc.substr(1));
 	var li = $('body>nav>ul[is=dropdown]>li>a[href="'+loc+'"]').parent('li');
