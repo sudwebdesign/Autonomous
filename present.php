3bec2601e2b3b5f1361f5b8f64585668b8786f3a
Jo <jo@wildsurikat.com>
2014-06-15 19:59:48 +0200

    more consistent presentation logic

--- present.php
+++ present.php
@@ -3,10 +3,13 @@ use surikat\control\session;
 use model\Table_Taxonomy;
 class present extends surikat\present{
 	function assign(){
-		$this->title				= (($u=view::param(0))?$u.' - ':'').'Autonomie et Partage';
+		$this->timeCompiled			= time();
 		$this->taxonomyRessource	= Table_Taxonomy::getChildrenbyLabel('Ressource');
 		$this->taxonomyEvenement	= Table_Taxonomy::getChildrenbyLabel('Évènement');
-		$this->timeCompiled			= time();
+		$this->BASE_HREF = 'http://'.$_SERVER['SERVER_NAME'].'/';
+		$this->URI = view::param(0);
+		$this->HREF = $this->BASE_HREF.$this->URI;
+		$this->title				= (($u=view::param(0))?$u.' - ':'').'Autonomie et Partage';
 	}
 	function dynamic(){
 		$this->time = time();
