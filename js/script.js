4dfef6ce7bbcd0aae402379eed8eed5d6fbdcce5
Jo <jo@wildsurikat.com>
2014-06-16 14:50:39 +0200

    active menu ajouter bugfix + other approch

--- js/script.js
+++ js/script.js
@@ -8,21 +8,25 @@ $js('jquery',function(){
 		}
 		return loc;
 	};
+	var splitterOnce = function(loc,splitters){		
+		for(var i in splitters){
+			var idf = loc.indexOf(splitters[i]);
+			if(idf>-1){
+				var x = loc.split(splitters[i]);
+				loc = x[0]+splitters[i]+x[1];
+			}
+		}
+		return loc;
+	}
 	var location = document.location.pathname;
 	location = decodeURIComponent(location.substr(1));
-	var splitters = ['|','/',':'];
-	var loc = splitter(location,splitters);
-	var splitOnce = splitters.shift();
-	var loc2 = splitter(location,splitters);
-	var idf = loc2.indexOf(splitOnce);
-	if(idf>-1){
-		var x = loc2.split(splitOnce);
-		loc2 = x[0]+splitOnce+x[1];
-	}
-	console.log(loc2);
-	$('body>nav>ul[is=dropdown]>li:has(>a[href^="'+loc+'"])'
-		+',body>nav>ul[is=dropdown]>li>ul>li:has(>a[href^="'+loc2+'"])'
-		+',body>footer>a[href="'+location+'"]').addClass('active');
+	var loc = splitter(location,['|','/',':','-']);
+	var loc2 = splitterOnce(location,['|','-']);
+	var selectorMenu = 'body>nav>ul[is=dropdown]>li:has(>a[href^="'+loc+'"])';
+	if(loc!=loc2)
+		selectorMenu += ',body>nav>ul[is=dropdown]>li>ul>li:has(>a[href^="'+loc2+'"])';
+	selectorMenu += ',body>footer>a[href="'+location+'"]';
+	$(selectorMenu).addClass('active');
 	
 	$(window).on('unload',function(){
 		$('main').css('opacity',0.5);
