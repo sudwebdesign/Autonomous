6cd769376490f44d19e7d3fad1936fa9bc7ed3d2
Jo <jo@wildsurikat.com>
2014-06-20 17:11:35 +0200

    wip

--- present.php
+++ present.php
@@ -6,13 +6,13 @@ class present extends surikat\present{
 		$this->timeCompiled			= time();
 		$this->taxonomyRessource	= Table_Taxonomy::getChildrenbyLabel('Ressource');
 		$this->taxonomyEvenement	= Table_Taxonomy::getChildrenbyLabel('Évènement');
-		$port = ($_SERVER['SERVER_PORT']&&$_SERVER['SERVER_PORT']!=80?':'.$_SERVER['SERVER_PORT']:'');
+		$port = ($_SERVER['SERVER_PORT']&&(int)$_SERVER['SERVER_PORT']!=80?':'.$_SERVER['SERVER_PORT']:'');
 		$this->BASE_HREF = 'http://'.$_SERVER['SERVER_NAME'].$port.'/';
+		$this->URI		= view::param(0);
+		$this->HREF		= $this->BASE_HREF.$this->URI;
 	}
 	function dynamic(){
 		$this->time		= time();
-		$this->URI		= view::param(0);
-		$this->HREF		= $this->BASE_HREF.$this->URI;
 		$this->title	= (($u=view::param(0))?$u.' - ':'').'Autonomie et Partage';
 		$this->h1		= $this->title;
 	}
