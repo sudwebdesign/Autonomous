9d0bcdda61a0fd2ef0cb91828c03f5ebbba57985
Jo <jo@wildsurikat.com>
2014-06-24 16:00:49 +0200

    add point type with new RedBean convention to geopoint

--- model/Table_Geopoint.php
+++ model/Table_Geopoint.php
@@ -1,10 +1,8 @@
 <?php namespace model;
 final class Table_Geopoint extends Table{
-	static $metaCast = array(
-		//'lat'=>'float', //trying with auto (double)
-		//'lng'=>'float',
-		//'point'=>'point',
-	);
+	var $columnPointCast	= 'point';
+	//var $columnPointWrite	= 'GeomFromText'; //MySql
+	//var $columnPointRead	= 'asText'; //MySql
 	//function onNew(){}
 	//function onCreate(){}
 	//function onDelete(){}
@@ -16,10 +14,11 @@ final class Table_Geopoint extends Table{
 	function onValidate(){
 		parent::onValidate();
 		if($this->lat!==false&&!($this->lat<=90.0&&$this->lat>=-90.0))
-			$this->error('lat','La latitude doit être comprise entre 90 et -90');
+			$this->error('xownGeopoint.lat','La latitude doit être comprise entre 90 et -90');
 		if($this->lng!==false&&!($this->lng<=180.0&&$this->lng>=-180.0))
-			$this->error('lng','La logintude doit être comprise entre 180 et -180');
-		//$this->point = 'POINT('.$this->lat.' '.$this->lng.')';
+			$this->error('xownGeopoint.lng','La logintude doit être comprise entre 180 et -180');
+		//$this->point = 'POINT('.$this->lat.' '.$this->lng.')'; //MySql
+		$this->point = '('.$this->lat.','.$this->lng.')'; //PgSql
 	}
 	//function onCreated(){}
 	//function onUpdated(){}
