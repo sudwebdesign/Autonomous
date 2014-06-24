721abbb2fbb901bba9ec58fa8d220d17c0f0ca6d
thomas <thomas@sudwebdesign.fr>
2014-06-24 06:36:19 +0200

    Mise a jour - Mise en page - items  (objet,article)
    Premier jet pour l'affichage d'un objet simple d'un utilisateur et
    au propriétés Communes aux  ressource, projet, Asso, Média, annonces,
    etc.
    Les singularités spécifiques de chaque objet restes indéterminées pour
    le moment.

--- present/item.php
+++ present/item.php
@@ -17,6 +17,14 @@ class item extends \present{
 		$params = array(
 			view::param(2),
 		);
-		$this->row = model::row4D($t,$query,$params);
+		$this->raw = model::row4D($t,$query,$params);
+		$imgFolder = 'content/'.$this->taxonomy.'/'.view::param(2).'/';
+		$imgName=str_replace(' ','-',$this->title);
+		$imgsItem = glob($imgFolder."{".$imgName.".jpg,".$imgName.".gif,".$imgName.".png}", GLOB_BRACE);
+		$this->srcimg = $imgsItem;
+		$this->presentation = $this->raw['presentation'];
+		$this->tel = $this->raw['tel'];
+		$this->lien = $this->raw['url'];
+		$this->création = $this->raw['created'];
 	}
 }
