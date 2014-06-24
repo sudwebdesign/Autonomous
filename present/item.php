735e7d3eca9ffb425a85185811c07a2c033f7d4b
thomas <thomas@sudwebdesign.fr>
2014-06-24 08:52:48 +0200

    Ajout style + icones sur les pages article (template item)
    c'est plus jolis

--- present/item.php
+++ present/item.php
@@ -17,6 +17,7 @@ class item extends \present{
 		$params = array(
 			view::param(2),
 		);
+		//var_dump(parent::$this);
 		$this->raw = model::row4D($t,$query,$params);
 		$imgFolder = 'content/'.$this->taxonomy.'/'.view::param(2).'/';
 		$imgName=str_replace(' ','-',$this->title);
@@ -26,5 +27,24 @@ class item extends \present{
 		$this->tel = $this->raw['tel'];
 		$this->lien = $this->raw['url'];
 		$this->création = $this->raw['created'];
+		
+		
+		
+		
+		/* Tests for adding automaticly var in presenter
+		$this->information_item=array();
+		foreach($this->raw as $k=>$firstLevel){
+			//$this->in.$k=$firstLevel;
+			
+			////$this->$k[$k]=$firstLevel;///cKK
+			$this->information_item[$k]=$firstLevel;
+			
+			//$this->$k=$firstLevel;//modifie taxonomie
+			//$this->taxonomy = $t;
+			
+			
+		}
+		* */
+
 	}
 }
