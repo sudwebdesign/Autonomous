5b95e719ca18311b28366d802e4610e1fb105d9a
Jo <jo@wildsurikat.com>
2014-07-15 13:47:16 +0200

    wip - erm

--- x-dom/graph-erm.js
+++ x-dom/graph-erm.js
@@ -1,96 +1,144 @@
-$css('joint');
-$js(true,['joint','joint.shapes.erd'],function(){
+$css('qtip');
+$js(true,[
+	'jquery',
+	'qtip',
+	'cytoscape',
+	'cytoscape-qtip',
+],function(){
 	$('graph-erm').each(function(){
-		var paperDom = $('#paper');
+		
 		// create the model for the E-R diagram
 		var nodeDataArray = [];
 		var linkDataArray = [];
 		var THIS = $(this);
 		THIS.find('table').each(function(){
-			var items = [];
+			var cols = [];
 			$(this).find('col').each(function(){
-				var name = $(this).attr('name');
-				var figure = $(this).attr('figure');
-				var color = $(this).attr('color');
-				if(!figure)
-					figure = 'MagneticData'; //MagneticData,Cube1,Cube2,Decision,TriangleUp
-				if(!color)
-					color = 'purple';
-				items.push({
-					'name':name,
-					'iskey':!!$(this).attr('iskey'),
-					'figure':figure,
-					'color':color,
-				});
+				cols.push($(this).attr('name'));
 			});
+			
+			var name = $(this).attr('name');
+			var figure = $(this).attr('figure');
+			var color = $(this).attr('color');
+			if(!figure)
+				figure = 'ellipse'; //ellipse,rectangle,octagon,triangle
+			if(!color)
+				color = '#86B342';
 			nodeDataArray.push({
-				'key':$(this).attr('name'),
-				'items':items
+				'data':{
+					'id':name,
+					'name':name,
+					//'weight':$(this).attr('weight'),
+					//'faveColor':color,
+					//'faveShape':figure
+					'theCols':cols
+				},
 			});
-		});
+		}).remove();
 		THIS.find('link').each(function(){
+			var strength = $(this).attr('strength');
+			//if(!strength)
+				//strength = 40;
 			linkDataArray.push({
-				'from':$(this).attr('from'),
-				'to':$(this).attr('to'),
-				'text':$(this).attr('text'),
-				'toText':$(this).attr('toText'),
+				'data':{
+					'source':$(this).attr('from'),
+					'target':$(this).attr('to'),
+					//'strength':strength,
+				}
 			});
-		});
+		}).remove();
 		//console.log(nodeDataArray);
 		//console.log(linkDataArray);
-
-		var graph = new joint.dia.Graph;
-
-		var paper = new joint.dia.Paper({
-			el: paperDom,
-			width: 800,
-			height: 600,
-			gridSize: 1,
-			model: graph,
-			interactive: false
+		
+		$('#myDiagram').cytoscape({
+			style: cytoscape.stylesheet()
+				.selector('node')
+					.css({
+						'content': 'data(name)',
+						'text-valign': 'center',
+						'color': 'white',
+						'text-outline-width': 1,
+						'text-outline-color': '#888',
+						'height': '20px',
+						'width': '20px',
+					})
+				.selector('edge')
+					.css({
+						'target-arrow-shape': 'triangle'
+					})
+				.selector(':selected')
+					.css({
+						'background-color': 'black',
+						'line-color': 'black',
+						'target-arrow-color': 'black',
+						'source-arrow-color': 'black'
+					})
+				.selector('.faded')
+					.css({
+						'opacity': 0.4,
+						'text-opacity': 0.1
+					}),
+			elements: {
+				nodes: nodeDataArray,
+				edges: linkDataArray
+			},
+			layout: {
+				name: 				'cose',
+				ready               : function() {},
+				stop                : function() {},
+				refresh             : 0, // Number of iterations between consecutive screen positions update (0 -> only updated on the end)
+				fit                 : true,  // Whether to fit the network view after when done
+				padding             : 30,  // Padding on fit
+				randomize           : true, // Whether to randomize node positions on the beginning
+				debug               : false, // Whether to use the JS console to print debug messages
+				nodeRepulsion       : 10000, // Node repulsion (non overlapping) multiplier
+				nodeOverlap         : 100, // Node repulsion (overlapping) multiplier
+				idealEdgeLength     : 10, // Ideal edge (non nested) length
+				edgeElasticity      : 100, // Divisor to compute edge forces
+				nestingFactor       : 5,  // Nesting factor (multiplier) to compute ideal edge length for nested edges
+				gravity             : 250,  // Gravity force (constant)
+				numIter             : 100, // Maximum number of iterations to perform
+				initialTemp         : 200, // Initial temperature (maximum node displacement)
+				coolingFactor       : 0.95,  // Cooling factor (how the temperature is reduced between consecutive iterations
+				minTemp             : 1 // Lower temperature threshold (below this point the layout will end)
+			},
+			ready: function(){
+				var cy = this;
+				cy.elements().unselectify();
+				cy.on('tap', 'node', function(e){
+					var node = e.cyTarget; 
+					var neighborhood = node.neighborhood().add(node);
+					cy.elements().addClass('faded');
+					neighborhood.removeClass('faded');
+				});
+				cy.on('tap', function(e){
+					if(e.cyTarget===cy)
+						cy.elements().removeClass('faded');
+				});
+				for(var i in nodeDataArray){
+					var v = nodeDataArray[i].data;
+					var html = '';
+					//html += v.name+':';
+					if(v.theCols&&v.theCols.length)
+						html += '<ul class="erm-column"><li>'+v.theCols.join('</li><li>')+'</li></ul>';
+					if(html)
+						cy.elements('#'+v.id).qtip({
+							content: html,
+							position: {
+								my: 'top center',
+								at: 'bottom center'
+							},
+							style: {
+								classes: 'qtip-bootstrap',
+								tip: {
+									width: 16,
+									height: 8
+								}
+							}
+						});
+				}
+			}
 		});
-
-		var erd = joint.shapes.erd;
-
-		var element = function(elm, x, y, label) {
-			var cell = new elm({ position: { x: x, y: y }, attrs: { text: { text: label }}});
-			graph.addCell(cell);
-			return cell;
-		};
-
-		var link = function(elm1, elm2) {
-			var myLink = new erd.Line({ source: { id: elm1.id }, target: { id: elm2.id }});
-			graph.addCell(myLink);
-			return myLink;
-		};
-
-		var employee = element(erd.Entity, 100, 200, "Employee");
-		var salesman = element(erd.Entity, 100, 400, "Salesman");
-		var wage = element(erd.WeakEntity, 530, 200, "Wage");
-		var paid = element(erd.IdentifyingRelationship, 350, 190, "gets paid");
-		var isa = element(erd.ISA, 125, 300, "ISA");
-		var number = element(erd.Key, 0, 90, "number");
-		var nameEl = element(erd.Normal, 75, 30, "name");
-		var skills = element(erd.Multivalued, 150, 90, "skills");
-		var amount = element(erd.Derived, 440, 80, "amount");
-		var date = element(erd.Normal, 590, 80, "date");
-		var plate = element(erd.Key, 405, 500, "plate");
-		var car = element(erd.Entity, 430, 400, "Company car");
-		var uses = element(erd.Relationship, 300, 390, "uses");
-
-		link(employee, paid).cardinality('1');
-		link(employee, number);
-		link(employee, nameEl);
-		link(employee, skills);
-		link(employee, isa);
-		link(isa, salesman);
-		link(salesman, uses).cardinality('0..1');;
-		link(car, uses).cardinality('1..1');
-		link(car, plate);
-		link(wage, paid).cardinality('N');
-		link(wage, amount);
-		link(wage, date);
-
-		paper.$el.css('pointer-events', 'none'); //freeze
+		
 	});
 });
