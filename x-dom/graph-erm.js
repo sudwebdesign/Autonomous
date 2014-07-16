ec2c0293ff5b4274315ad67068f2d61534e249b8
Jo <jo@wildsurikat.com>
2014-07-16 23:40:29 +0200

    erm visualitation end + update surikat

--- x-dom/graph-erm.js
+++ x-dom/graph-erm.js
@@ -22,55 +22,58 @@ $js(true,[
 					'id':name,
 					'name':name,
 					'theCols':cols
-				},
+				}
 			});
 		});
 		THIS.find('link').each(function(){
 			linkDataArray.push({
 				'data':{
 					'source':$(this).attr('from'),
-					'target':$(this).attr('to')
+					'target':$(this).attr('to'),
+					'relation':$(this).attr('relation')
 				}
 			});
 		});
 		//console.log(nodeDataArray);
 		//console.log(linkDataArray);
-		
-		$(this).empty().cytoscape({
-			style: cytoscape.stylesheet()
-				.selector('node')
-					.css({
-						'content': 'data(name)',
-						'font-size': '0.8em',
-						'text-valign': 'center',
-						'color': '#FFF',
-						'text-outline-width': 0.2,
-						'text-outline-color': '#888',
-						'height': '5em',
-						'width': '5em',
-						'background-color': '#86B342',
-					})
-				.selector('edge')
-					.css({
-						'target-arrow-shape': 'triangle'
-					})
-				.selector(':selected')
-					.css({
-						'background-color': 'black',
-						'line-color': 'black',
-						'target-arrow-color': 'black',
-						'source-arrow-color': 'black'
-					})
-				.selector('.faded')
-					.css({
-						'opacity': 0.4,
-						'text-opacity': 0.1
-					}),
-			elements: {
-				nodes: nodeDataArray,
-				edges: linkDataArray
+
+		var layouts = {
+			concentric: {
+				name: 'concentric',
+				fit: true, // whether to fit the viewport to the graph
+				ready: undefined, // callback on layoutready
+				stop: undefined, // callback on layoutstop
+				padding: 30, // the padding on fit
+				startAngle: 3/2 * Math.PI, // the position of the first node
+				counterclockwise: false, // whether the layout should go counterclockwise (true) or clockwise (false)
+				minNodeSpacing: 10, // min spacing between outside of nodes (used for radius adjustment)
+				height: undefined, // height of layout area (overrides container height)
+				width: undefined, // width of layout area (overrides container width)
+				concentric: function(){ // returns numeric value for each node, placing higher nodes in levels towards the centre
+				  return this.degree();
+				},
+				levelWidth: function(nodes){ // the variation of concentric values in each level
+				  return nodes.maxDegree() / 4;
+				}
 			},
-			layout: {
+			breadthfirst: {
+				name: 'breadthfirst',
+				fit: true, // whether to fit the viewport to the graph
+				directed: false, // whether the tree is directed downwards (or edges can point in any direction if false)
+				padding: 30, // padding on fit
+				circle: false, // put depths in concentric circles if true, put depths top down if false
+				roots: undefined, // the roots of the trees
+				maximalAdjustments: 0 // how many times to try to position the nodes in a maximal way (i.e. no backtracking)
+			},
+			circle: {
+				name: 'circle',
+				fit: true, // whether to fit the viewport to the graph
+				rStepSize: 10, // the step size for increasing the radius if the nodes don't fit on screen
+				padding: 30, // the padding on fit
+				startAngle: 3/2 * Math.PI, // the position of the first node
+				counterclockwise: false // whether the layout should go counterclockwise (true) or clockwise (false)
+			},
+			cose: {
 				name: 				'cose',
 				ready               : function() {},
 				stop                : function() {},
@@ -84,12 +87,56 @@ $js(true,[
 				idealEdgeLength     : 10, // Ideal edge (non nested) length
 				edgeElasticity      : 10, // Divisor to compute edge forces
 				nestingFactor       : 5,  // Nesting factor (multiplier) to compute ideal edge length for nested edges
-				gravity             : 250,  // Gravity force (constant)
+				gravity             : 450,  // Gravity force (constant)
 				numIter             : 100, // Maximum number of iterations to perform
 				initialTemp         : 200, // Initial temperature (maximum node displacement)
 				coolingFactor       : 0.95,  // Cooling factor (how the temperature is reduced between consecutive iterations
 				minTemp             : 1 // Lower temperature threshold (below this point the layout will end)
 			},
+		};
+		for(var k in layouts)
+			$(this).before('<a class="layout-apply" data-layout="'+k+'">'+k+'</a>');
+		
+		var elements = {
+			nodes: nodeDataArray,
+			edges: linkDataArray
+		};
+		$(this).empty().cytoscape({
+			style: cytoscape.stylesheet()
+				.selector('node')
+					.css({
+						'content': 'data(name)',
+						'font-size': '1em',
+						'text-valign': 'center',
+						'color': '#FFF',
+						'text-outline-width': 0.2,
+						'text-outline-color': '#888',
+						'height': '6em',
+						'width': '6em',
+						'background-color': '#86B342',
+					})
+				.selector('edge')
+					.css({
+						'target-arrow-shape': 'triangle',
+						'color': '#888',
+					})
+				.selector('edge[relation="shared"]')
+					.css({
+						'source-arrow-shape': 'triangle'
+					})
+				.selector(':selected')
+					.css({
+						'background-color': 'black',
+						'line-color': 'black',
+						'target-arrow-color': 'black',
+						'source-arrow-color': 'black'
+					})
+				.selector('.faded')
+					.css({
+						'opacity': 0,
+						'text-opacity': 0
+					}),
+			elements: elements,
 			ready: function(){
 				var cy = this;
 				cy.elements().unselectify();
@@ -106,11 +153,11 @@ $js(true,[
 				for(var i in nodeDataArray){
 					var v = nodeDataArray[i].data;
 					var html = '';
-					//html += v.name+':';
 					if(v.theCols&&v.theCols.length)
 						html += '<ul class="erm-column"><li>'+v.theCols.join('</li><li>')+'</li></ul>';
+					var el = cy.elements('#'+v.id);
 					if(html)
-						cy.elements('#'+v.id).qtip({
+						el.qtip({
 							content: html,
 							position: {
 								my: 'top center',
@@ -125,8 +172,15 @@ $js(true,[
 							}
 						});
 				}
+				cy.layout(layouts['breadthfirst']);
+				$('a.layout-apply').click(function(e){
+					e.preventDefault();
+					var k = $(this).attr('data-layout');
+					cy.layout(layouts[k]);
+					return false;
+				});
 			}
-		});
+		});	
 		
 	});
 });
