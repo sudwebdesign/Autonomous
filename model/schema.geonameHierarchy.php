15e1ef2e4271c9b3776b2ed1e11f57772f4a5d80
Jo <jo@wildsurikat.com>
2014-07-19 16:20:06 +0200

    wip

--- model/schema.geonameHierarchy.php
+++
@@ -1,7 +0,0 @@
-<?php namespace model;
-use control;
-use control\CsvIterator;
-$csvIterator = new CsvIterator(control::$CWD.'.data/geonameHierarchy.csv',"\t");
-foreach($csvIterator as $i=>$line){
-	var_dump($line);exit;
-}
