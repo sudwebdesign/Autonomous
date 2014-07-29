13c24ee02d1adb07ce697b72802335ac13e02b8a
Jo <jo@wildsurikat.com>
2014-07-29 16:59:18 +0200

    taxonomy ManyToMany OK

--- model/Table_Taxonomy.php
+++ model/Table_Taxonomy.php
@@ -17,7 +17,7 @@ final class Table_Taxonomy extends ATable_Keyword{
 	//function onDeleted(){}
 	static function getChildrenbyLabel($params=null){
 		if($params)
-			return model::getAssoc('taxonomy',array('where'=>array('taxonomy_id=(SELECT id FROM taxonomy WHERE name=?)',array($params))));
+			return model::getAssoc('taxonomy',array('joinOn'=>'taxonomy','where'=>array('taxonomy_taxonomy.taxonomy2_id=(SELECT id FROM taxonomy WHERE name=?)',array($params))));
 		else
 			return model::getAssoc('taxonomy',array('where'=>'taxonomy_id IS NULL'));
 	}
