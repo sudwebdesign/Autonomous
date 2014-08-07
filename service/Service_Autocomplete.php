4c36908d1756bee543f10c98e5adea32d8cb5eaf
Jo <jo@wildsurikat.com>
2014-08-07 22:59:06 +0200

    wip

--- service/Service_Autocomplete.php
+++ service/Service_Autocomplete.php
@@ -8,14 +8,13 @@ class Service_Autocomplete {
 	static function geoname(){
 		$results = array();
 		if(isset($_GET['term'])&&strlen($term=trim($_GET['term']))>=1){
-			$term = );
 			$results = model::newFrom('geoname')
 				->select('id')
 				->select('name')
 				->select('latitude')
 				->select('longitude')
 				//->select('radius')
-				->where('asciiname LIKE ?',array(strtolower(str::unaccent($term).'%'))
+				->where('asciiname LIKE ?',array(strtolower(str::unaccent($term).'%')))
 				->where('fcode = ?',array('ADM4'))
 				->getAll()
 			;
