44b2b6a8a5090bcd44d3ad51eb87e440517e49b8
Jo <jo@wildsurikat.com>
2014-08-07 17:24:14 +0200

    wip

--- x-dom/geo-completer.js
+++ x-dom/geo-completer.js
@@ -6,7 +6,7 @@ $js([
 ],function(){
 	geocallback = function(){
 		$('geo-completer').each(function(){
-			var geocompleter = $(this);
+			var geolocal = $(this).find('geo-local');
 			//$.getJSON('service/autocomplete/geoinit',function(params){
 			var params = <!--#include virtual="/service/autocomplete/geoinit" -->;
 			var defaultMapZoom = 17;
@@ -15,255 +15,207 @@ $js([
 				new google.maps.LatLng(params.northEastLatMainBound,params.northEastLngMainBound)
 			);
 			var geocoder = new google.maps.Geocoder();
-			var input_lat = geocompleter.find('input[type=number][step=any]:eq(0)');
-			var input_lng = geocompleter.find('input[type=number][step=any]:eq(1)');
-			var input_rayon = geocompleter.find('input[type=number][step][step!=any]:eq(0)');
-			var input = geocompleter.find('input[type=text][name*=label]');
-			var input_address = geocompleter.find('input[type=text][name*=address]');
-			var input_city = geocompleter.find('input[type=text][name*=city]');
-			var input_country = geocompleter.find('input[type=text][name*=country]');
-			var input_region = geocompleter.find('input[type=text][name*=arealevel1]');
-			var input_departement = geocompleter.find('input[type=text][name*=arealevel2]');
-			var input_validate = geocompleter.find('input[type=hidden]:eq(0)');
+			var input_lat = geolocal.find('input[type=number][step=any]:eq(0)');
+			var input_lng = geolocal.find('input[type=number][step=any]:eq(1)');
+			var input_rayon = geolocal.find('input[type=number][step][step!=any]:eq(0)');
+			var input = geolocal.find('input[type=text][name*=label]');
 			var div_map = $('<div class="map-canvas"></div>');
-			div_map.insertAfter(input);
-			var distance = function(lat1, lon1, lat2, lon2){
-				var R = 6371; // Radius of the earth in km
-				var dLat = (lat2 - lat1) * Math.PI / 180;  // deg2rad below
-				var dLon = (lon2 - lon1) * Math.PI / 180;
-				var a = 0.5 - Math.cos(dLat)/2 + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * (1 - Math.cos(dLon))/2;
-				return R * 2 * Math.asin(Math.sqrt(a));
-			};
-			var updateAdresse = function(latLng,updateMark){
-				geocoder.geocode({'latLng':latLng},function(results,status){
-					if(status=='OK'){
-						input.val(results[0].formatted_address);
-						updatePlace(results[0],updateMark);
-					}				
+			
+			var launch = function(){
+				div_map.insertAfter(input);
+				var distance = function(lat1, lon1, lat2, lon2){
+					var R = 6371; // Radius of the earth in km
+					var dLat = (lat2 - lat1) * Math.PI / 180;  // deg2rad below
+					var dLon = (lon2 - lon1) * Math.PI / 180;
+					var a = 0.5 - Math.cos(dLat)/2 + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * (1 - Math.cos(dLon))/2;
+					return R * 2 * Math.asin(Math.sqrt(a));
+				};
+				var updateAdresse = function(latLng,updateMark){
+					geocoder.geocode({'latLng':latLng},function(results,status){
+						if(status=='OK'){
+							input.val(results[0].formatted_address);
+							updatePlace(results[0],updateMark);
+						}				
+					});
+				};
+				var floatFromStr = function(v){
+					if(typeof(v)!='undefined')
+						return parseFloat(v.replace(',','.'));
+				};
+				var updatePosition = function(){
+					updateAdresse(new google.maps.LatLng(floatFromStr(input_lat.val()), floatFromStr(input_lng.val())),true);
+				};
+				var updateMarker = function(place){
+					marker.setVisible(false);
+					marker.setIcon({
+					  url: typeof(place.icon)!='undefined'?place.icon:'img/geocode.png',
+					  size: new google.maps.Size(71, 71),
+					  origin: new google.maps.Point(0, 0),
+					  anchor: new google.maps.Point(17, 34),
+					  scaledSize: new google.maps.Size(35, 35)
+					});
+					marker.setPosition(place.geometry.location);
+					marker.setVisible(true);
+				};
+				input.keypress(function(e){
+					if(e.which==13){
+						e.preventDefault();
+						input.trigger('focus');
+						input.simulate('keydown',{keyCode:$.ui.keyCode.DOWN}).simulate('keydown',{keyCode:$.ui.keyCode.ENTER});
+						return false;
+					}
 				});
-			};
-			var floatFromStr = function(v){
-				if(typeof(v)!='undefined')
-					return parseFloat(v.replace(',','.'));
-			};
-			var updatePosition = function(){
-				updateAdresse(new google.maps.LatLng(floatFromStr(input_lat.val()), floatFromStr(input_lng.val())),true);
-			};
-			var updateMarker = function(place){
-				marker.setVisible(false);
-				marker.setIcon({
-				  url: typeof(place.icon)!='undefined'?place.icon:'img/geocode.png',
-				  size: new google.maps.Size(71, 71),
-				  origin: new google.maps.Point(0, 0),
-				  anchor: new google.maps.Point(17, 34),
-				  scaledSize: new google.maps.Size(35, 35)
-				});
-				marker.setPosition(place.geometry.location);
-				marker.setVisible(true);
-			};
-			input.keypress(function(e){
-				if(e.which==13){
-					e.preventDefault();
-					input.trigger('focus');
-					input.simulate('keydown',{keyCode:$.ui.keyCode.DOWN}).simulate('keydown',{keyCode:$.ui.keyCode.ENTER});
-					return false;
-				}
-			});
-			input.on('input',function(){
-				input_lat.val('');
-				input_lng.val('');
-				input_rayon.val('');
-				input_validate.val('false');
-			});
-			//input_lat.on('input',updatePosition);
-			input_lng.on('input',updatePosition);
-			input_rayon.on('input',function(){
-				var val = $(this).val();
-				if(val){
-					circle.setRadius(floatFromStr(val)*1000.0);
-					circle.bindTo('center', marker, 'position');
-					circle.setVisible(true);
-				}
-				else{
-					circle.setVisible(false);
-				}
-			});
-			var map = new google.maps.Map(div_map.get(0),{ //https://developers.google.com/maps/documentation/javascript/reference?csw=1#MapOptions
-				zoom: 8,
-				mapTypeId: google.maps.MapTypeId.HYBRID
-				/*			
-					MapTypeId.ROADMAP displays the default road map view. This is the default map type.
-					MapTypeId.SATELLITE displays Google Earth satellite images
-					MapTypeId.HYBRID displays a mixture of normal and satellite views
-					MapTypeId.TERRAIN displays a physical map based on terrain information. 
-				*/,
-				center:new google.maps.LatLng(params.centerLatMainBound,params.centerLngMainBound)
-			});
-			//map.fitBounds(bounds);
-			map.controls[google.maps.ControlPosition.TOP_LEFT].push(input.get(0));
-			var autocomplete = new google.maps.places.Autocomplete(input.get(0),{
-				bounds:bounds,
-				region:params.region,
-				componentRestrictions:{
-					country:params.country
-				}
-				/* type: //https://developers.google.com/places/documentation/supported_types */
-			});
-			//autocomplete.bindTo('bounds', map);
-			var infowindow = new google.maps.InfoWindow();
-			var marker = new google.maps.Marker({map: map,draggable:true});
-			var circle = new google.maps.Circle({ //https://developers.google.com/maps/documentation/javascript/reference?csw=1#CircleOptions
-			  //visible: false,
-			  map: map,
-			  fillColor: '#AA0000',
-			  fillOpacity: 0.5,
-			  strokeOpacity:1,
-			  strokeWeight:1,
-			  strokeColor:'#000',
-			  editable:true
-			});
-			var setRayon = function(place){
-				if(place&&place.geometry&&place.geometry.viewport&&(place.types[0]=="locality"||place.types[0]=="administrative_area_level_2")){
-					var center = place.geometry.viewport.getCenter();
-					var northEast = place.geometry.viewport.getNorthEast();
-					var southWest = place.geometry.viewport.getSouthWest();
-					var lat = center.lat();
-					var lng = center.lng();
-					var r = (distance(lat,lng,northEast.lat(),northEast.lng())+distance(lat,lng,southWest.lat(),southWest.lng()))/2.0;
-					input_rayon.val(r);
-					circle.setRadius(r*1000.0);
-					circle.bindTo('center', marker, 'position');
-					circle.setVisible(true);
-				}
-				else{
-					circle.setVisible(false);
+				input.on('input',function(){
+					input_lat.val('');
+					input_lng.val('');
 					input_rayon.val('');
-				}
-			};
-			var updatePlace = function(place,updateMark){
-				setRayon(place);
-				if(typeof(place)!='object'||!place.geometry)
-					return;
-				if(place.geometry.viewport){
-					map.fitBounds(place.geometry.viewport);
-				}
-				else{
-					map.setCenter(place.geometry.location);
-					map.setZoom(defaultMapZoom);
-				}
-				if(updateMark)
-					updateMarker(place);
-
-				console.log(place);
-				
-				if(input_lat.val()!=place.geometry.location.lat())
-					input_lat.val(place.geometry.location.lat());
-				if(input_lng.val()!=place.geometry.location.lng())
-					input_lng.val(place.geometry.location.lng());
-					
-				input_validate.val('true');
-
-				var address_components = place.address_components;
-				var areas = [];
-				for(var i in address_components){
-					var compo = address_components[i];
-					switch(compo.types[0]){
-						case 'locality':
-							input_city.val(compo.long_name);
-							areas.push(compo.long_name);
-						break;
-						case 'administrative_area_level_1':								
-							input_region.val(compo.long_name);
-							areas.push(compo.long_name);
-						break;
-						case 'administrative_area_level_2':
-							input_departement.val(compo.long_name);
-							areas.push(compo.long_name);
-						break;
-						case 'country':
-							input_country.val(compo.long_name);
-							areas.push(compo.long_name);
-						break;
-						//case 'postal_code':
-						//break;
-						//case 'route':
-						//break;
-						//case 'sublocality':
-						//break;
-						//case 'street_number':
-						//break;
-
-					}
-				}
-				var x = input.val().split(',');
-				var address = '';
-				for(var i in x){
-					x[i] = x[i].trim();
-					if(areas.indexOf(x[i].trim())===-1)
-						address += x[i]+' ';
-				}
-				address = address.trim();
-				input_address.val(address);
-				//console.log(place);
-				
-				input.trigger('change');
-			};
-			//var autocompleteService = new google.maps.places.AutocompleteService();
-			google.maps.event.addListener(circle, 'radius_changed', function(){
-				var val = circle.getRadius()/1000.0;
-				if(val!=floatFromStr(input_rayon.val()))
-					input_rayon.val(val);
-			});
-			google.maps.event.addListener(marker, 'dragstart', function(e){
-				circle.setVisible(false);
-				infowindow.close();
-			});
-			google.maps.event.addListener(marker, 'dragend', function(e){
-				var latLng = e.latLng;
-				input_lat.val(latLng.lat());
-				input_lng.val(latLng.lng());
-				updateAdresse(latLng);
-			});
-			google.maps.event.addListener(map, 'dragend', function(){
-				var center = map.getCenter();
-				marker.setPosition(center);
-				input_rayon.val('');
-				circle.setVisible(false);
-				input_lat.val(center.lat());
-				input_lng.val(center.lng());
-				map.setZoom(defaultMapZoom);
-				geocoder.geocode({'latLng':center},function(results,status){
-					if(status=='OK'){
-						input.val(results[0].formatted_address);
-						input_validate.val('true');
-					}
 				});
-			});
-			google.maps.event.addListener(autocomplete, 'place_changed', function(){
-				var place = autocomplete.getPlace();
-				if(typeof(place)=='object'&&place.geometry){
-					if(place.geometry.viewport){
-						var center = place.geometry.viewport.getCenter();
-						input_lat.val(center.lat());
-						input_lng.val(center.lng());
+				//input_lat.on('input',updatePosition);
+				input_lng.on('input',updatePosition);
+				input_rayon.on('input',function(){
+					var val = $(this).val();
+					if(val){
+						circle.setRadius(floatFromStr(val)*1000.0);
+						circle.bindTo('center', marker, 'position');
+						circle.setVisible(true);
 					}
 					else{
-						input_lat.val(place.geometry.location.lat());
-						input_lng.val(place.geometry.location.lng());
-					}
-				}
-				updatePlace(place,true);
-				
-			});
-			var val = input.val();
-			if(val){
-				geocoder.geocode({'address':val},function(results,status){
-					if(status==='OK'){
-						input.val(results[0].formatted_address);
-						updatePlace(results[0],true);
+						circle.setVisible(false);
 					}
 				});
-			}
+				var map = new google.maps.Map(div_map.get(0),{ //https://developers.google.com/maps/documentation/javascript/reference?csw=1#MapOptions
+					zoom: 8,
+					mapTypeId: google.maps.MapTypeId.HYBRID
+					/*			
+						MapTypeId.ROADMAP displays the default road map view. This is the default map type.
+						MapTypeId.SATELLITE displays Google Earth satellite images
+						MapTypeId.HYBRID displays a mixture of normal and satellite views
+						MapTypeId.TERRAIN displays a physical map based on terrain information. 
+					*/,
+					center:new google.maps.LatLng(params.centerLatMainBound,params.centerLngMainBound)
+				});
+				//map.fitBounds(bounds);
+				map.controls[google.maps.ControlPosition.TOP_LEFT].push(input.get(0));
+				var autocomplete = new google.maps.places.Autocomplete(input.get(0),{
+					bounds:bounds,
+					region:params.region,
+					componentRestrictions:{
+						country:params.country
+					}
+					/* type: //https://developers.google.com/places/documentation/supported_types */
+				});
+				//autocomplete.bindTo('bounds', map);
+				var infowindow = new google.maps.InfoWindow();
+				var marker = new google.maps.Marker({map: map,draggable:true});
+				var circle = new google.maps.Circle({ //https://developers.google.com/maps/documentation/javascript/reference?csw=1#CircleOptions
+				  //visible: false,
+				  map: map,
+				  fillColor: '#AA0000',
+				  fillOpacity: 0.5,
+				  strokeOpacity:1,
+				  strokeWeight:1,
+				  strokeColor:'#000',
+				  editable:true
+				});
+				var setRayon = function(place){
+					if(place&&place.geometry&&place.geometry.viewport&&(place.types[0]=="locality"||place.types[0]=="administrative_area_level_2")){
+						var center = place.geometry.viewport.getCenter();
+						var northEast = place.geometry.viewport.getNorthEast();
+						var southWest = place.geometry.viewport.getSouthWest();
+						var lat = center.lat();
+						var lng = center.lng();
+						var r = (distance(lat,lng,northEast.lat(),northEast.lng())+distance(lat,lng,southWest.lat(),southWest.lng()))/2.0;
+						input_rayon.val(r);
+						circle.setRadius(r*1000.0);
+						circle.bindTo('center', marker, 'position');
+						circle.setVisible(true);
+					}
+					else{
+						circle.setVisible(false);
+						input_rayon.val('');
+					}
+				};
+				var updatePlace = function(place,updateMark){
+					setRayon(place);
+					if(typeof(place)!='object'||!place.geometry)
+						return;
+					if(place.geometry.viewport){
+						map.fitBounds(place.geometry.viewport);
+					}
+					else{
+						map.setCenter(place.geometry.location);
+						map.setZoom(defaultMapZoom);
+					}
+					if(updateMark)
+						updateMarker(place);
+					
+					if(input_lat.val()!=place.geometry.location.lat())
+						input_lat.val(place.geometry.location.lat());
+					if(input_lng.val()!=place.geometry.location.lng())
+						input_lng.val(place.geometry.location.lng());
+					input.trigger('change');
+				};
+				//var autocompleteService = new google.maps.places.AutocompleteService();
+				google.maps.event.addListener(circle, 'radius_changed', function(){
+					var val = circle.getRadius()/1000.0;
+					if(val!=floatFromStr(input_rayon.val()))
+						input_rayon.val(val);
+				});
+				google.maps.event.addListener(marker, 'dragstart', function(e){
+					circle.setVisible(false);
+					infowindow.close();
+				});
+				google.maps.event.addListener(marker, 'dragend', function(e){
+					var latLng = e.latLng;
+					input_lat.val(latLng.lat());
+					input_lng.val(latLng.lng());
+					updateAdresse(latLng);
+				});
+				google.maps.event.addListener(map, 'dragend', function(){
+					var center = map.getCenter();
+					marker.setPosition(center);
+					input_rayon.val('');
+					circle.setVisible(false);
+					input_lat.val(center.lat());
+					input_lng.val(center.lng());
+					map.setZoom(defaultMapZoom);
+					geocoder.geocode({'latLng':center},function(results,status){
+						if(status=='OK'){
+							input.val(results[0].formatted_address);
+						}
+					});
+				});
+				google.maps.event.addListener(autocomplete, 'place_changed', function(){
+					var place = autocomplete.getPlace();
+					if(typeof(place)=='object'&&place.geometry){
+						if(place.geometry.viewport){
+							var center = place.geometry.viewport.getCenter();
+							input_lat.val(center.lat());
+							input_lng.val(center.lng());
+						}
+						else{
+							input_lat.val(place.geometry.location.lat());
+							input_lng.val(place.geometry.location.lng());
+						}
+					}
+					updatePlace(place,true);
+					
+				});
+				var val = input.val();
+				if(val){
+					geocoder.geocode({'address':val},function(results,status){
+						if(status==='OK'){
+							input.val(results[0].formatted_address);
+							updatePlace(results[0],true);
+						}
+					});
+				}
+				
+			};
+
+			launch();
+			
+			
 		});
 	};
 	$js('http://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places&callback=geocallback');
