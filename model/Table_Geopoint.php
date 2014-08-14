619e1835dab42763c9bff5506adf3b44f0f3fc63
Jo <jo@wildsurikat.com>
2014-08-14 13:32:48 +0200

    Mixin Geopoint + bounding box for boost geocoding
    bounding box "normalized denormalization"

--- model/Table_Geopoint.php
+++ model/Table_Geopoint.php
@@ -1,27 +1,13 @@
 <?php namespace model;
-final class Table_Geopoint extends Table{	
-	static $columnPointCast	= 'point';
-	//var $columnPointWriteCol	= 'GeomFromText'; //MySql
-	//var $columnPointReadCol	= 'asText'; //MySql
-	//function onNew(){}
-	//function onCreate(){}
-	//function onDelete(){}
-	function onUpdate(){
-		parent::onUpdate();
-		$this->lat = $this->lat!=''?(float)$this->lat:false;
-		$this->lon = $this->lon!=''?(float)$this->lon:false;
-	}
+final class Table_Geopoint extends Table{
+	use Mixin_Geopoint;
 	function onValidate(){
-		parent::onValidate();
-		if($this->lat!==false&&!($this->lat<=90.0&&$this->lat>=-90.0))
+		if(!$this->checkLat($this->lat))
 			$this->error('xownGeopoint.lat','La latitude doit être comprise entre 90 et -90');
-		if($this->lon!==false&&!($this->lon<=180.0&&$this->lon>=-180.0))
+		if(!$this->checkLon($this->lon))
 			$this->error('xownGeopoint.lon','La logintude doit être comprise entre 180 et -180');
-		//$this->point = 'POINT('.$this->lat.' '.$this->lon.')'; //MySql
-		$this->point = '('.$this->lat.','.$this->lon.')'; //PgSql
+		$this->setPoint($this->lat,$this->lon);
+		if($this->radius)
+			$this->setBounds($this->lat,$this->lon,$this->radius);
 	}
-	//function onCreated(){}
-	//function onUpdated(){}
-	//function onDeleted(){}
-	//function onRead(){}
 }
