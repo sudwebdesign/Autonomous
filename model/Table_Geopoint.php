1f8150a6c6c2dd5cf6a6cfe73f4160e43368aca0
Jo <jo@wildsurikat.com>
2014-08-14 13:35:18 +0200

    orthograph correction

--- model/Table_Geopoint.php
+++ model/Table_Geopoint.php
@@ -5,7 +5,7 @@ final class Table_Geopoint extends Table{
 		if(!$this->checkLat($this->lat))
 			$this->error('xownGeopoint.lat','La latitude doit être comprise entre 90 et -90');
 		if(!$this->checkLon($this->lon))
-			$this->error('xownGeopoint.lon','La logintude doit être comprise entre 180 et -180');
+			$this->error('xownGeopoint.lon','La longitude doit être comprise entre 180 et -180');
 		$this->setPoint($this->lat,$this->lon);
 		if($this->radius)
 			$this->setBounds($this->lat,$this->lon,$this->radius);
