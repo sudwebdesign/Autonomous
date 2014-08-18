20648f0c8ed2b4f95fd9542440431e332829edad
Jo <jo@wildsurikat.com>
2014-08-18 18:22:46 +0200

    re-replace select2 by tagsinput

--- x-dom/select2.js
+++ x-dom/select2.js
@@ -10,34 +10,34 @@ $js(true,[
 			dropdownAutoWidth:true,
 			width:'100%',
 			//placeholder: "",
-			minimumInputLength: 1,
-			ajax: {
-				url: '',
-				dataType: 'jsonp',
-				data: function(term, page){
-					return {
-						q: term, // search term
-						page_limit: 10
-					};
-				},
-				results: function(data, page){
-					return {results: data.movies};
-				}
-			},
-			initSelection: function(element, callback) {
+			//minimumInputLength: 1,
+			//ajax: {
+				//url: '',
+				//dataType: 'jsonp',
+				//data: function(term, page){
+					//return {
+						//q: term, // search term
+						//page_limit: 10
+					//};
+				//},
+				//results: function(data, page){
+					//return {results: data.movies};
+				//}
+			//},
+			//initSelection: function(element, callback) {
 				// the input tag has a value attribute preloaded that points to a preselected movie's id
 				// this function resolves that id attribute to an object that select2 can render
 				// using its formatResult renderer - that way the movie name is shown preselected
-				var id = $(element).val();
-				if(id!==''){
-					$.ajax("", {
-						data: {
-							
-						},
-						dataType: 'jsonp'
-					}).done(function(data) { callback(data); });
-				}
-			},
+				//var id = $(element).val();
+				//if(id!==''){
+					//$.ajax("", {
+						//data: {
+							//
+						//},
+						//dataType: 'jsonp'
+					//}).done(function(data) { callback(data); });
+				//}
+			//},
 			//formatResult: ,
 			//formatSelection: , 
 			//dropdownCssClass: "bigdrop", // apply css that makes the dropdown taller
