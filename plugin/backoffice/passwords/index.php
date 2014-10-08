420538e7b1dd84e2f4e5788e26e44236cdc478e9
surikat <jo@surikat.pro>
2014-10-08 13:09:23 +0200

    cleaning

--- plugin/backoffice/passwords/index.php
+++
@@ -1,2 +0,0 @@
-<iframe src="./htpasswd.php" style="float:left;width:46%;height:100%;border:0;"></iframe>
-<iframe src="./superadmin.php" style="float:left;width:46%;height:100%;border:0;"></iframe>
--- plugin/backoffice/simplepo/SimplePO_PoMsgStore.php
+++
@@ -1,6 +0,0 @@
-<?php
-interface SimplePO_PoMsgStore {
-  public function write( $msg, $isHeader );
-	public function read();
-}
-?>
