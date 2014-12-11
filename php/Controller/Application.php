f7f2df169715ceb1348daa76f94f2050b78b3aae
surikat <jo@surikat.pro>
2014-12-11 01:44:20 +0100

    cleaning

--- php/Controller/Application.php
+++ php/Controller/Application.php
@@ -1,10 +1,5 @@
 <?php namespace Controller;
-use I18n\Lang;
 class Application extends \Surikat\Controller\Application{
-	function run($path){
-		Lang::setLocale('fr');
-		parent::run($path);
-	}
 	function filterParam($path){
 		return $this->Router->filterParam($path);
 	}
