261217a08884a4be6f551645d5332a97bcb447c9
surikat <jo@surikat.pro>
2015-01-04 00:16:44 +0100

    update

--- index.php
+++ index.php
@@ -2,13 +2,18 @@
 if(!@include(__DIR__.'/Surikat/Loader.php'))
 	symlink('../Surikat','Surikat')&&include('Surikat/Loader.php');
 use Core\Dev;
-Dev::level(
-	//Dev::CONTROL
-	Dev::STD
-	//|Dev::ROUTE
-	//|Dev::MODEL
-	|Dev::CSS
-	|Dev::JS
-	//|Dev::IMG
-);
+//Dev::on(Dev::PHP);
+//Dev::on(Dev::CONTROL);
+//Dev::on(Dev::VIEW);
+//Dev::on(Dev::PRESENT);
+Dev::on(Dev::MODEL);
+//Dev::on(Dev::MODEL_SCHEMA);
+//Dev::on(Dev::ROUTE);
+//Dev::on(Dev::I18N);
+//Dev::on(Dev::IMG);
+//Dev::on(Dev::SERVER);	//PHP+CONTROL+VIEW+PRESENT+MODEL+MODEL_SCHEMA+I18N
+//Dev::on(Dev::NAV);	//URI+JS+CSS+IMG
+Dev::on(Dev::STD);		//PHP+CONTROL+VIEW+PRESENT+MODEL_SCHEMA+I18N
+Dev::on(Dev::CSS);
+Dev::on(Dev::JS);
 (new Controller\Application())->run(@$_SERVER['PATH_INFO']);
