aad53417103a07030b8b547dd41aa24ae9e1be1b
surikat <jo@surikat.pro>
2015-01-10 02:04:22 +0100

    put dipatcher index at right place

--- php/Dispatcher/Index.php
+++ php/Dispatcher/Index.php
@@ -2,7 +2,7 @@
 use I18n\Lang;
 class Index extends \Surikat\Dispatcher\Index{
 	function setHooks(){
-		setlocale (LC_ALL, 'fr', 'fr_FR', 'fr_FR.utf8', 'fra');
+		setlocale(LC_ALL, 'fr', 'fr_FR', 'fr_FR.utf8', 'fra');
 		Lang::set('fr');
 	}
 }
