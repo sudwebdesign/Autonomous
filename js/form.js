618c5c258ee73d748e79e3a06c500afc7ad8a052
surikat <jo@surikat.pro>
2015-01-28 21:09:37 +0100

    wip

--- js/form.js
+++ js/form.js
@@ -1,2 +1,6 @@
 <!--#include virtual="./form.memory.js" -->
 <!--#include virtual="./form.validation.js" -->
+$js('jquery',function(){
+	//$('input[name][required]').prev('legend').addClass('required');
+	$('input[name][required]').after('<div class="required" style="float:left;" />');
+});
