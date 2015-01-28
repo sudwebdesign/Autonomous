c84be01623365c0ca97c9829384058c778237a7f
surikat <jo@surikat.pro>
2015-01-29 00:29:44 +0100

    wip

--- js/is.login-box.js
+++ js/is.login-box.js
@@ -1,22 +1,23 @@
 $css('is.login-box');
-$js('jquery',function(){
-	$('[is="login-box"]').show();
+$css('jquery-ui/core');
+$css('jquery-ui/button');
+$css('jquery-ui/dialog');
+$js(['jquery',
+	'jquery-ui/core',
+	'jquery-ui/widget',
+	'jquery-ui/button',
+	'jquery-ui/dialog'
+],true,function(){
+	$('body').append('<div class="login-dialog"></div>');
+	var login = $('.login-dialog');
+	var dialog = login.dialog({
+		autoOpen:false
+	});
 	$.get('login-box',function(html){
-		$('body').append('<div class="login-popup"><a href="#" class="close"><img src="img/close_pop.png" class="btn_close" title="Close Window" alt="Close"></a>'+html+'</div><div class="mask"></div>')
-		$('header a[href="Login"]').click(function(e){
+		dialog.append(html);
+		$('[is="login-box"]').show().click(function(e){
 			e.preventDefault();
-			$('.login-popup').fadeIn(300);
-			var popMargTop = ($('.login-popup').height() + 24) / 2; 
-			var popMargLeft = ($('.login-popup').width() + 24) / 2; 
-			$('.login-popup').css({ 
-				'margin-top' : -popMargTop,
-				'margin-left' : -popMargLeft
-			});
-			$('.mask').fadeIn(300);
-			return false;
-		});
-		$('a.close, .mask').click(function(){
-			$('.mask , .login-popup').fadeOut(300);
+			dialog.dialog('open');
 			return false;
 		});
 	});
