316cf479cb726a58891ced50c70ffc359ecb9b8b
surikat <jo@surikat.pro>
2015-01-29 00:37:59 +0100

    wip

--- js/is.login-box.js
+++ js/is.login-box.js
@@ -8,10 +8,11 @@ $js(['jquery',
 	'jquery-ui/button',
 	'jquery-ui/dialog'
 ],true,function(){
-	$('body').append('<div class="login-dialog"></div>');
+	$('body').append('<div class="login-dialog" title="Login"></div>');
 	var login = $('.login-dialog');
 	var dialog = login.dialog({
-		autoOpen:false
+		autoOpen:false,
+		modal:true
 	});
 	$.get('login-box',function(html){
 		dialog.append(html);
